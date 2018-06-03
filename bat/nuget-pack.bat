fd933861b75f6c71d91b5ae0ad0eaa3116954b25
blqw.zhou <fttl_398@126.com>
2018-06-03 20:55:25 +0800

    Update nuget-pack.bat

--- bat/nuget-pack.bat
+++ bat/nuget-pack.bat
@@ -1,6 +1,6 @@
 del %CD%\unpkgs\*.nupkg
 dotnet pack -c Release -o %CD%\unpkgs
 for /r . %%a in (unpkgs\*.nupkg) do (
- 	nuget push "%%a" -source https://www.nuget.org/api/v2/package -apikey %1%
+ 	dotnet nuget push "%%a" -s https://api.nuget.org/v3/index.json -k %1%
 )
 del %CD%\unpkgs\*.nupkg
